6e5b656f88e1f8d6a8c1731c0d6ff0ec13f037cc
David Gitman <wereking@gmail.com>
2020-12-13 20:12:26 +0200

    add a new basic test file

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,28 @@
+def userInput
+
+pipeline {
+    agent any
+
+    stages {
+        stage('Input') {
+            steps {
+                script {
+                    userInput = input message: 'Please provide your input', ok: 'confirm', parameters: [choice(name: '', choices: ['option 1', 'option2'], description: '')]
+                }
+            }
+        }
+        stage('Hello') {
+            steps {
+                dir('CoolNewDirectory') {
+                    git branch: 'main', credentialsId: 'github_cred', url: 'https://github.com/dvdgitman/IP-Geolocation-Locator.git'
+                    echo 'Hello World'
+                }
+            }
+        }
+        stage('Print Inputed string') {
+            steps {
+                println("Input was " + userInput)
+            }
+        }
+    }
+}
