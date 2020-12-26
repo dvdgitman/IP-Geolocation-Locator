c484df5fea64820114f4e9cc1d8b28842a4dcfae
David Gitman <wereking@gmail.com>
2020-12-26 17:42:15 +0200

    added test

---
+++ BasicTest/basic.test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+check_result () {
+  RESULT=$?
+  MESSAGE=$1
+  if [ $RESULT == 0 ]; then
+    echo [SUCCESS] $MESSAGE
+  else
+    echo [FAILED] $MESSAGE
+    exit 1
+  fi
+}
+
+for sentiment in City Country Region ; do
+  docker run iplocation -i 8.8.8.8 | grep $sentiment
+  check_result $sentiment
+done
+
+exit 0
