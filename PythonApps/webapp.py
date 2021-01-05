a005172449e0b7cabcd946f0518ba425a32c276f
David Gitman <wereking@gmail.com>
2021-01-05 20:45:41 +0200

    changed jenkins file

--- PythonApps/webapp.py
+++ PythonApps/webapp.py
@@ -4,13 +4,13 @@ from PythonApps import iplocator
 
 app = Flask(__name__)
 
-@app.route("/ip", methods=["GET", "POST"])
+@app.route("/ip")
 
-def hello():
- return "Here we will check geolocation from an IP address"
+def ip():
+ return iplocator.response
 
 if __name__ == "__main__":
     app.run(debug=True,port=5000)
 
-set FLASK_APP=iplocator.py
+
 
