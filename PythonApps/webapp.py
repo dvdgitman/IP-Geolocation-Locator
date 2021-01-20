b65a9d03304d1288429e8a748014932f02f6721b
David Gitman <wereking@gmail.com>
2021-01-20 04:40:08 +0200

    added flask

--- PythonApps/webapp.py
+++ PythonApps/webapp.py
@@ -4,16 +4,13 @@ from flask import jsonify
 
 app = Flask(__name__)
 
+FLASK_ENV = '-i 1.1.1.1'
+
 
 @app.route("/ip", methods=['GET', 'POST'])
 def ip():
     return jsonify("IP:" + iplocator.ip, "City:" + iplocator.city,
                    "Region:" + iplocator.region, "Country:" + iplocator.country)
-
-
-@app.route("/ip2", methods=['GET', 'POST'])
-def ip2():
-    return iplocator.response
 
 
 if __name__ == "__main__":
