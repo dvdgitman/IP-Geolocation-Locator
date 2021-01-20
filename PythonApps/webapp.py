d8d4d527b5dc0fe7351923496187b1a1c1647aa8
David Gitman <wereking@gmail.com>
2021-01-20 05:17:40 +0200

    added flask

--- PythonApps/webapp.py
+++ PythonApps/webapp.py
@@ -1,10 +1,8 @@
 from flask import Flask
-from PythonApps import iplocator
+from . import iplocator
 from flask import jsonify
 
 app = Flask(__name__)
-
-FLASK_ENV = '-i 1.1.1.1'
 
 
 @app.route("/ip", methods=['GET', 'POST'])
